6543017d04ce8b03c91563978ff01c5315e51479
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-14 23:20:01 +0500

    Add Student class

---
+++ Students.CommandLine/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Students.CommandLine
+{
+    static class Program
+    {
+        private static void Main()
+        {
+            const string path = @"C:\Projects\Students";
+            const string filename = "students.xml";
+
+            var s1 = new Student {FirstName = "Ivan", SecondName = "Korotkov", Faculty = "Math"};
+            var serializer = new XmlSerializer(typeof(Student));
+            using (var stream = new FileStream(Path.Combine(path, filename), FileMode.Create))
+            {
+                serializer.Serialize(stream, s1);
+            }
+        }
+    }
+}
---
+++ Students/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Students
+{
+    [Serializable]
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Faculty { get; set; }
+    }
+}
