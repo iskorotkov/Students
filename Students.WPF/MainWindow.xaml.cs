98d984a29c2e44b2ede8ae10fd1cb269e91e012f
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-15 07:58:00 +0500

    Add creating new students and saving them in xml file

--- Students.WPF/MainWindow.xaml.cs
+++ Students.WPF/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -9,9 +9,63 @@ namespace Students.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Student> _students = new List<Student>();
+        private Student _selectedStudent;
+        private StudentSerializer _serializer = new StudentSerializer();
+        
         public MainWindow()
         {
             InitializeComponent();
+            FirstNameBox.TextChanged += UpdateFirstName;
+            SecondNameBox.TextChanged += UpdateSecondName;
+            FacultyBox.TextChanged += UpdateFaculty;
+        }
+
+        private void NewList_OnClick(object sender, RoutedEventArgs e)
+        {
+            _students.Clear();
+            ClearForm();
+        }
+
+        private void Add_OnClick(object sender, RoutedEventArgs e)
+        {
+            _selectedStudent = new Student();
+            _students.Add(_selectedStudent);
+            UpdateForm();
+        }
+
+        private void UpdateFaculty(object sender, TextChangedEventArgs e)
+        {
+            _selectedStudent.Faculty = FacultyBox.Text;
+        }
+
+        private void UpdateSecondName(object sender, TextChangedEventArgs e)
+        {
+            _selectedStudent.SecondName = SecondNameBox.Text;
+        }
+
+        private void UpdateFirstName(object sender, TextChangedEventArgs e)
+        {
+            _selectedStudent.FirstName = FirstNameBox.Text;
+        }
+
+        private void ClearForm()
+        {
+            FirstNameBox.Text = string.Empty;
+            SecondNameBox.Text = string.Empty;
+            FacultyBox.Text = string.Empty;
+        }
+
+        private void UpdateForm()
+        {
+            FirstNameBox.Text = _selectedStudent.FirstName;
+            SecondNameBox.Text = _selectedStudent.SecondName;
+            FacultyBox.Text = _selectedStudent.Faculty;
+        }
+
+        private void Save_OnClick(object sender, RoutedEventArgs e)
+        {
+            _serializer.Serialize(_students);
         }
     }
 }
---
+++ Students.WPF/StudentSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Students.WPF
+{
+    public class StudentSerializer
+    {
+        private const string Path = @"C:\Projects\Students";
+        private const string Filename = "students.xml";
+        
+        public void Serialize(Student student)
+        {
+            var serializer = new XmlSerializer(typeof(Student));
+            using var stream = new FileStream(System.IO.Path.Combine(Path, Filename), FileMode.Create);
+            serializer.Serialize(stream, student);
+        }
+
+        public void Serialize(List<Student> students)
+        {
+            var serializer = new XmlSerializer(typeof(List<Student>));
+            using var stream = new FileStream(System.IO.Path.Combine(Path, Filename), FileMode.Create);
+            serializer.Serialize(stream, students);
+        }
+    }
+}
