e7a3f981f06cab992a3e9fd2581673d94f2948ea
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-16 21:19:19 +0500

    Add MakeMaster() to Bachelor class

--- Students/Bachelor.cs
+++ Students/Bachelor.cs
@@ -5,5 +5,19 @@ namespace Students
     [Serializable]
     public class Bachelor : Student
     {
+        public Master MakeMaster()
+        {
+            return new Master
+            {
+                FirstName = FirstName,
+                SecondName = SecondName,
+                Faculty = Faculty,
+                Degree = new Degree
+                {
+                    Domain = Faculty,
+                    Year = DateTime.Today.Year
+                }
+            };
+        }
     }
 }
